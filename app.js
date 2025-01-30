d9e9129046e149008e94fe379d3fa1730ce268a3
Helbert Enoch <agredaenoch@gmail.com>
2025-01-29 19:23:33 -0500

    Avances en la app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+ 
+    let friend = document.getElementById('amigo').value.trim();
+
+    if (friend) {
+        amigos.push(friend);
+        console.log(amigos);
+        limpiarCaja();
+        mostrarAmigos(amigos);
+    } else {
+        alert('Por favor, inserte un nombre')
+    }
+}
+
+function mostrarAmigos(amigos) {
+    let listaAmigos = asignarTextoElemento("listaAmigos", amigos)
+
+    for (let i = 0, len = amigos.length; i < len; i++) {
+        let amigo = amigos[i];
+        let elemento = document.createElement('li');
+        elemento.textContent = amigo;
+        elemento.id = `amigo${i}`;
+    }
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
